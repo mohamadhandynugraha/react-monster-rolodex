e4e204e72bc23ae08b6572affd79e24c7033656b
Nyool <handynugraha29@gmail.com>
2019-08-31 13:29:12 +0700

    sekarang udah sampai video 24. Besok di malang video 25 react udemy Andrei.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters : []
+    }
+  }
+
+  // ambil data dari API jsonplaceholder
+  async componentDidMount(){
+    const responseUsers = await fetch('https://jsonplaceholder.typicode.com/users');
+    const jsonUsers = await responseUsers.json();
+    this.setState({monsters: jsonUsers})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => {
+          return (
+            <h1 key={monster.id}>Hello, {monster.name}</h1>
+          );
+        })}
+        <button onClick={() => this.setState({monsters: []})}>Change the name in index 0</button>
+      </div>
+    );
+  }
 }
 
 export default App;
